7c1f2a28e0581a2c990ace06023d98bf1c67b1d3
bchun001 <chunweijiebenjamin@gmail.com>
2024-03-19 10:19:45 +0800

    tmp: added controller

--- server/controller.cpp
+++ server/controller.cpp
@@ -28,59 +28,53 @@ HashMap globalHashMap;
 int main() {
     while (true)
     {
-        // // TODO test connection with marshaller
-        // // construct connection module object
-        // ConnectionModule conn(8080);
+        // TODO test connection with marshaller
+        // construct connection module object
+        ConnectionModule conn(8080);
 
-        // // Accept incoming connections
-        // int clientSocket = conn.waitForConnection();
-        // if (clientSocket == -1) {
-        //     return 1; // Exit if accepting connection fails
-        // }
-        // // return 2;
+        // Accept incoming connections
+        int clientSocket = conn.waitForConnection();
+        if (clientSocket == -1) {
+            return 1; // Exit if accepting connection fails
+        }
+        // return 2;
 
-        // // Receive data
-        // char receiveBuffer[1024]; // Assuming a maximum buffer size of 1024 bytes
-        // if (conn.receiveData(receiveBuffer, sizeof(receiveBuffer)) == false) {
-        //     return 1; // Exit if receiving fails
-        // }
+        // Receive data
+        char receiveBuffer[1024]; // Assuming a maximum buffer size of 1024 bytes
+        if (conn.receiveData(receiveBuffer, sizeof(receiveBuffer)) == false) {
+            return 1; // Exit if receiving fails
+        }
+
+        // TODO Marshaller
+        // Instantiate marshaller
+        Marshaller marshaller;
 
         // pass thru unmarshaller
-        // TODO
+        Request requestObject = Marshaller.unmarshal(receiveBuffer);
 
         // initialise buffer to store return value from services
         std::vector<char> buffer(MAX_RESPONSE_BUFFER_LENGTH);
 
-        // TEST to remove: Create a ReadRequest object
-        MonitorRequest requestObject = MonitorRequest("1234", 3, "file.txt", 1000);
-        // monitor file.txt
-        requestObject.process(globalHashMap);
-        // create readrequest
-        ReadRequest requestObject2 = ReadRequest("1234", 1, "file.txt", 0, 10);
-        // read and check if file is being monitored
-        requestObject2.process();
-        requestObject2.checkMonitor(globalHashMap);
-        globalHashMap.printAll();
-
         // TODO: Iron out with jx on the return var
-        // // read
-        // if (typeid(requestObject)==typeid(ReadRequest)) {
-        //     requestObject.process();
-        //     requestObject.checkMonitor(globalHashMap);
-        //     globalHashMap.printAll();
-        // }
-        // // write
-        // else if (typeid(requestObject)==typeid(WriteRequest)){
-        //     requestObject.process();
-        // }
-        // // monitor
-        // else if (typeid(requestObject)==typeid(MonitorRequest)){
-        //     requestObject.process();
-        // }
-        // else {
-        //     // print error message opcode does not exist
-        //     std::cout << "Error: opcode " << requestObject.opcode << " does not exist." << std::endl;
-        // }
+        // TODO: change the request interface so that everything is common
+        // read
+        if (typeid(requestObject)==typeid(ReadRequest)) {
+            requestObject.process();
+            requestObject.checkMonitor(globalHashMap);
+            globalHashMap.printAll();
+        }
+        // write
+        else if (typeid(requestObject)==typeid(WriteRequest)){
+            requestObject.process();
+        }
+        // monitor
+        else if (typeid(requestObject)==typeid(MonitorRequest)){
+            requestObject.process();
+        }
+        else {
+            // print error message opcode does not exist
+            std::cout << "Error: opcode " << requestObject.opcode << " does not exist." << std::endl;
+        }
 
         // after going thru the server service
 
